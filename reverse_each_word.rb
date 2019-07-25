9ff1c76a976907470368e62f3ecc8064e7c38abe
Jinai Franklin <jinai.franklin@yahoo.com>
2019-07-25 03:36:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,3 +8,14 @@ def reverse_each_word(x)
 end
 
 reverse_each_word("Hello there, and how are you?")
+
+
+def reverse_each_word_c(str)
+  c_array = str.split(" ")
+  two_array = []
+  c_array.collect do |str|
+    two_array << str.reverse
+  end
+  two_array.join(" ")
+end
+
