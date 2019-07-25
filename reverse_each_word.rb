fab89e9a34afcbeb6739225996cbd3bbbb7084d6
Jinai Franklin <jinai.franklin@yahoo.com>
2019-07-25 03:29:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(x)
   array = x.split(" ")
   new_array = []
-  array.each do |x|
-    new_array << x.reverse_each_word
+  array.each do |string|
+    new_array << x.reverse
   end
   new_array.join(" ")
 end
